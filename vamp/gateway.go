4cda881ac4a18a0a411097f00fe97ca3103ce71b
Vincent Lesierse <vincent@lesierse.com>
2019-03-13 23:45:14 +0100

    Add Vamp Client API & Gateway service

---
+++ vamp/gateway.go
@@ -0,0 +1,127 @@
+package vamp
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
+
+type GatewayService service
+
+type IGatewayService interface {
+	Get(name string) (*Gateway, *Response, error)
+	List() ([]*Gateway, *Response, error)
+	Update(gw *Gateway) (*Gateway, *Response, error)
+}
+
+type Gateway struct {
+	Name         string                 `json:"name,omitempty"`
+	Metadata     map[string]interface{} `json:"metadata"`
+	Selector     string                 `json:"selector,omitempty"`
+	VirtualHosts []string               `json:"virtual_hosts"`
+	Routes       map[string]Route       `json:"routes"`
+
+	Kind       string `json:"kind"`
+	Port       string `json:"port"`
+	Sticky     string `json:"sticky,omitempty"`
+	Internal   bool   `json:"internal"`
+	Deployed   bool   `json:"deployed"`
+	LookupName string `json:"lookup_name"`
+	Service    struct {
+		Host string `json:"host"`
+		Port string `json:"port"`
+	} `json:"service"`
+}
+
+type Route struct {
+	LookupName string                 `json:"lookup_name"`
+	Metadata   map[string]interface{} `json:"metadata"`
+	Balance    string                 `json:"balance"`
+	Rewrites   []struct {
+		Path string `json:"path"`
+	} `json:"rewrites"`
+	Weight            int    `json:"-"`
+	Condition         string `json:"-"`
+	ConditionStrength int    `json:"-"`
+}
+
+func (r Route) MarshalJSON() ([]byte, error) {
+	type Alias Route
+	return json.Marshal(&struct {
+		WeightRaw            string `json:"weight"`
+		ConditionRaw         string `json:"condition"`
+		ConditionStrengthRaw string `json:"condition_strength"`
+		Alias
+	}{
+		ConditionRaw:         r.Condition,
+		WeightRaw:            fmt.Sprintf("%d%%", r.Weight),
+		ConditionStrengthRaw: fmt.Sprintf("%d%%", r.ConditionStrength),
+		Alias:                (Alias)(r),
+	})
+}
+
+func (r *Route) UnmarshalJSON(data []byte) error {
+	type Alias Route
+	aux := &struct {
+		WeightRaw            string            `json:"weight"`
+		ConditionRaw         map[string]string `json:"condition"`
+		ConditionStrengthRaw string            `json:"condition_strength"`
+		Alias
+	}{
+		Alias: (Alias)(*r),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+	if aux.ConditionRaw != nil {
+		r.Condition = aux.ConditionRaw["condition"]
+	}
+	r.Weight, _ = strconv.Atoi(aux.WeightRaw[:len(aux.WeightRaw)-1])
+	r.ConditionStrength, _ = strconv.Atoi(aux.ConditionStrengthRaw[:len(aux.ConditionStrengthRaw)-1])
+	return nil
+}
+
+func (s *GatewayService) Get(name string) (*Gateway, *Response, error) {
+	u := fmt.Sprintf("gateways/%s", name)
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	gateway := new(Gateway)
+	response, err := s.client.Do(req, gateway)
+	if err != nil {
+		return nil, response, err
+	}
+	return gateway, response, nil
+}
+
+func (s *GatewayService) Update(gw *Gateway) (*Gateway, *Response, error) {
+	u := fmt.Sprintf("gateways/%s", gw.Name)
+	req, err := s.client.NewRequest("PUT", u, gw)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	gateway := new(Gateway)
+	response, err := s.client.Do(req, gateway)
+	if err != nil {
+		return nil, response, err
+	}
+	return gateway, response, nil
+}
+
+func (s *GatewayService) List() ([]*Gateway, *Response, error) {
+	u := fmt.Sprintf("gateways")
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var gateways []*Gateway
+	response, err := s.client.Do(req, &gateways)
+	if err != nil {
+		return nil, response, err
+	}
+	return gateways, response, nil
+}
---
+++ vamp/vamp.go
@@ -0,0 +1,145 @@
+package vamp
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+const DefaultUserAgent = "Vamp SDK (Go)"
+
+type Client struct {
+	httpClient *http.Client
+
+	BaseURL   *url.URL
+	UserAgent string
+	Token     string
+
+	common service
+
+	Gateway IGatewayService
+}
+
+type Response struct {
+	*http.Response
+}
+
+type ErrorResponse struct {
+	Response *http.Response
+	Message  string `json:"message"`
+}
+
+func (r *ErrorResponse) Error() string {
+	return fmt.Sprintf("%v %v: %d %v",
+		r.Response.Request.Method, r.Response.Request.URL,
+		r.Response.StatusCode, r.Message)
+}
+
+type service struct {
+	client *Client
+}
+
+func NewClient(baseURL string, namespace string, token string) (*Client, error) {
+	baseEndpoint, err := url.Parse(baseURL)
+	if err != nil {
+		return nil, err
+	}
+	if !strings.HasSuffix(baseEndpoint.Path, "/") {
+		baseEndpoint.Path += "/"
+	}
+	baseEndpoint.Path += fmt.Sprintf("%s/api/v1/", namespace)
+
+	c := &Client{httpClient: http.DefaultClient, UserAgent: DefaultUserAgent}
+	c.Token = token
+	c.BaseURL = baseEndpoint
+	c.common.client = c
+	c.Gateway = IGatewayService((*GatewayService)(&c.common))
+	return c, nil
+}
+
+func (c *Client) NewRequest(method, path string, body interface{}) (*http.Request, error) {
+	if !strings.HasSuffix(c.BaseURL.Path, "/") {
+		return nil, fmt.Errorf("BaseURL must have a trailing slash, but %q does not", c.BaseURL)
+	}
+
+	u, err := c.BaseURL.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+	req.Header.Set("Accept", "application/json")
+	if c.Token != "" {
+		req.Header.Set("Authorization", "token "+c.Token)
+	}
+	if c.UserAgent != "" {
+		req.Header.Set("User-Agent", c.UserAgent)
+	}
+	return req, nil
+}
+func (c *Client) Do(req *http.Request, v interface{}) (*Response, error) {
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	response := newResponse(resp)
+
+	err = CheckResponse(resp)
+	if err != nil {
+		return response, err
+	}
+
+	if v != nil {
+		if w, ok := v.(io.Writer); ok {
+			io.Copy(w, resp.Body)
+		} else {
+			decErr := json.NewDecoder(resp.Body).Decode(v)
+			if decErr == io.EOF {
+				decErr = nil // ignore EOF errors caused by empty response body
+			}
+			if decErr != nil {
+				err = decErr
+			}
+		}
+	}
+
+	return response, err
+}
+
+func CheckResponse(r *http.Response) error {
+	if c := r.StatusCode; 200 <= c && c <= 299 {
+		return nil
+	}
+	errorResponse := &ErrorResponse{Response: r}
+	data, err := ioutil.ReadAll(r.Body)
+	if err == nil && data != nil {
+		json.Unmarshal(data, errorResponse)
+	}
+	return errorResponse
+}
+
+func newResponse(r *http.Response) *Response {
+	response := &Response{Response: r}
+	return response
+}
