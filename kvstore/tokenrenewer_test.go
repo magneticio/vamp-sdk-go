a456fcd18edf9d78f10ca5302e4515d88822d512
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2020-10-12 10:27:12 +0200

    refactored token renewer tests

--- kvstore/tokenrenewer_test.go
+++ kvstore/tokenrenewer_test.go
@@ -20,72 +20,62 @@ var vaultToken = getVaultToken()
 
 func TestIntegrationTokenRenewer(t *testing.T) {
 
-	Convey("Given token renewer with renewal interval set to 0", t, func() {
+	Convey("Given Vault client with valid token", t, func() {
 		client, _ := api.NewClient(&api.Config{
 			Address: vaultAddress,
 		})
 		client.SetToken(vaultToken)
 
-		_, err := NewTokenRenewer(client, 0, 2*time.Second)
-
-		Convey("error should be thrown", func() {
-			So(err.Error(), ShouldEqual, "Token TTL and token renewal interval must be greater than 0")
-		})
-	})
-
-	Convey("Given token renewer with token TTL set to 0", t, func() {
-		client, _ := api.NewClient(&api.Config{
-			Address: vaultAddress,
-		})
-		client.SetToken(vaultToken)
-
-		_, err := NewTokenRenewer(client, time.Second, 0)
-
-		Convey("error should be thrown", func() {
-			So(err.Error(), ShouldEqual, "Token TTL and token renewal interval must be greater than 0")
-		})
-	})
-
-	Convey("Given token renewer with token TTL lower then renewal interval", t, func() {
-		client, _ := api.NewClient(&api.Config{
-			Address: vaultAddress,
-		})
-		client.SetToken(vaultToken)
-
-		_, err := NewTokenRenewer(client, 2*time.Second, time.Second)
-
-		Convey("error should be thrown", func() {
-			So(err.Error(), ShouldEqual, "Token TTL must be greater than token renewal interval")
-		})
-	})
-
-	Convey("Given token renewer for client with empty token", t, func() {
-		client, _ := api.NewClient(&api.Config{
-			Address: vaultAddress,
-		})
-		tokenRenewer, _ := NewTokenRenewer(client, time.Second, 2*time.Second)
-
-		Convey("When renewing token", func() {
-			err := tokenRenewer.renewToken()
+		Convey("When creating token renewer with renewal interval set to 0", func() {
+			_, err := NewTokenRenewer(client, 0, 2*time.Second)
 
 			Convey("error should be thrown", func() {
-				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldEqual, "Token TTL and token renewal interval must be greater than 0")
+			})
+		})
+
+		Convey("When creating token renewer with token TTL set to 0", func() {
+			_, err := NewTokenRenewer(client, time.Second, 0)
+
+			Convey("error should be thrown", func() {
+				So(err.Error(), ShouldEqual, "Token TTL and token renewal interval must be greater than 0")
+			})
+		})
+
+		Convey("When creating token renewer with token TTL lower then renewal interval", func() {
+			_, err := NewTokenRenewer(client, 2*time.Second, time.Second)
+
+			Convey("error should be thrown", func() {
+				So(err.Error(), ShouldEqual, "Token TTL must be greater than token renewal interval")
+			})
+		})
+
+		Convey("and valid token renewer", func() {
+			tokenRenewer, _ := NewTokenRenewer(client, time.Second, 2*time.Second)
+
+			Convey("When renewing token", func() {
+				err := tokenRenewer.renewToken()
+
+				Convey("error should not be thrown", func() {
+					So(err, ShouldBeNil)
+				})
 			})
 		})
 	})
 
-	Convey("Given valid token renewer", t, func() {
+	Convey("Given Vault client without a token", t, func() {
 		client, _ := api.NewClient(&api.Config{
 			Address: vaultAddress,
 		})
-		client.SetToken(vaultToken)
-		tokenRenewer, _ := NewTokenRenewer(client, time.Second, 2*time.Second)
+		Convey("and valid token renewer", func() {
+			tokenRenewer, _ := NewTokenRenewer(client, time.Second, 2*time.Second)
 
-		Convey("When renewing token", func() {
-			err := tokenRenewer.renewToken()
+			Convey("When renewing token", func() {
+				err := tokenRenewer.renewToken()
 
-			Convey("error should not be thrown", func() {
-				So(err, ShouldBeNil)
+				Convey("error should be thrown", func() {
+					So(err, ShouldNotBeNil)
+				})
 			})
 		})
 	})
